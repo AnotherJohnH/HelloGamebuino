372eee52fc51dd7f41a7d8d74bd61d2ed14cf1bc
John D. Haughton <anotherjohnh@gmail.com>
2021-02-20 16:28:33 +0000

    Updated build to be more like the Arduino build - hopefully fix the 'Does not work issue'

--- gamebuino.cmake
+++ gamebuino.cmake
@@ -12,22 +12,27 @@ set(ARDUINO_ROOT $ENV{ARDUINO_ROOT})
 
 set(CMAKE_SYSTEM_NAME Generic)
 
-include_directories(Gamebuino-Classic)
 include_directories("${ARDUINO_ROOT}/arduino/avr/cores/arduino")
 include_directories("${ARDUINO_ROOT}/arduino/avr/variants/standard")
 include_directories("${ARDUINO_ROOT}/arduino/avr/libraries/SPI/src")
-include_directories("${ARDUINO_ROOT}/tools/avr/avr/include")
+include_directories("Gamebuino-Classic/")
+include_directories("Gamebuino-Classic/utility")
 
 set(CMAKE_BUILD_TYPE Release)
 
-set(CMAKE_C_FLAGS_RELEASE   "-g -Os -w \
-                             -fpermissive -fno-exceptions -ffunction-sections \
-                             -fdata-sections -fno-threadsafe-statics \
-                             -Wno-error=narrowing -MMD -mmcu=atmega328p \
+set(CMAKE_C_FLAGS_RELEASE   "-g -Os -w -std=gnu11 \
+                             -ffunction-sections -fdata-sections \
+                             -MMD -flto -fno-fat-lto-objects -mmcu=atmega328p \
                              -DF_CPU=16000000L -DARDUINO=10813 \
                              -DARDUINO_AVR_UNO -DARDUINO_ARCH_AVR")
 
-set(CMAKE_CXX_FLAGS_RELEASE "-std=gnu++11 ${CMAKE_C_FLAGS_RELEASE}")
+set(CMAKE_CXX_FLAGS_RELEASE "-g -Os -w -std=gnu++11 \
+                             -fpermissive -fno-exceptions -ffunction-sections \
+                             -fdata-sections -fno-threadsafe-statics \
+                             -Wno-error=narrowing \
+                             -MMD -flto -mmcu=atmega328p \
+                             -DF_CPU=16000000L -DARDUINO=10813 \
+                             -DARDUINO_AVR_UNO -DARDUINO_ARCH_AVR")
 
 set(bin_prefix              "${ARDUINO_ROOT}/tools/avr/bin/avr-")
 
@@ -40,55 +45,62 @@ set(CMAKE_OBJCOPY           ${bin_prefix}objcopy)
 set(CMAKE_OBJDUMP           ${bin_prefix}objdump)
 set(CMAKE_SIZE              ${bin_prefix}size)
 
-set(ld_flags "-w -Os -g -Wl,--gc-sections -mmcu=atmega328p")
+set(ld_flags "-w -Os -g -flto -fuse-linker-plugin -Wl,--gc-sections -mmcu=atmega328p")
 
 set(CMAKE_CXX_LINK_EXECUTABLE
-    "${bin_prefix}g++ ${ld_flags} <OBJECTS> -o <TARGET>.elf <LINK_LIBRARIES>; \
+    "${bin_prefix}gcc ${ld_flags} <OBJECTS> -o <TARGET>.elf <LINK_LIBRARIES>; \
      ${CMAKE_OBJCOPY} -O ihex -j .eeprom --set-section-flags=.eeprom=alloc,load --no-change-warnings --change-section-lma .eeprom=0 \
                      <TARGET>.elf <TARGET>.eep; \
      ${CMAKE_OBJCOPY} -O ihex -R .eeprom <TARGET>.elf <TARGET>.HEX; \
      ${CMAKE_SIZE} <TARGET>.elf")
 
 #-------------------------------------------------------------------------------
-# Build libarduino.a and libgamebuino.a libraries
 
-add_library(arduino
-            ${ARDUINO_ROOT}/arduino/avr/cores/arduino/main.cpp
-            ${ARDUINO_ROOT}/arduino/avr/cores/arduino/CDC.cpp
-            ${ARDUINO_ROOT}/arduino/avr/cores/arduino/HardwareSerial.cpp
-            ${ARDUINO_ROOT}/arduino/avr/cores/arduino/HardwareSerial0.cpp
-            ${ARDUINO_ROOT}/arduino/avr/cores/arduino/HardwareSerial1.cpp
-            ${ARDUINO_ROOT}/arduino/avr/cores/arduino/HardwareSerial2.cpp
-            ${ARDUINO_ROOT}/arduino/avr/cores/arduino/HardwareSerial3.cpp
-            ${ARDUINO_ROOT}/arduino/avr/cores/arduino/IPAddress.cpp
-            ${ARDUINO_ROOT}/arduino/avr/cores/arduino/PluggableUSB.cpp
-            ${ARDUINO_ROOT}/arduino/avr/cores/arduino/Print.cpp
-            ${ARDUINO_ROOT}/arduino/avr/cores/arduino/Stream.cpp
-            ${ARDUINO_ROOT}/arduino/avr/cores/arduino/Tone.cpp
-            ${ARDUINO_ROOT}/arduino/avr/cores/arduino/USBCore.cpp
-            ${ARDUINO_ROOT}/arduino/avr/cores/arduino/WInterrupts.c
-            ${ARDUINO_ROOT}/arduino/avr/cores/arduino/WMath.cpp
-            ${ARDUINO_ROOT}/arduino/avr/cores/arduino/WString.cpp
-            ${ARDUINO_ROOT}/arduino/avr/cores/arduino/abi.cpp
-            ${ARDUINO_ROOT}/arduino/avr/cores/arduino/hooks.c
-            ${ARDUINO_ROOT}/arduino/avr/cores/arduino/new.cpp
-            ${ARDUINO_ROOT}/arduino/avr/cores/arduino/wiring.c
-            ${ARDUINO_ROOT}/arduino/avr/cores/arduino/wiring_analog.c
-            ${ARDUINO_ROOT}/arduino/avr/cores/arduino/wiring_digital.c
-            ${ARDUINO_ROOT}/arduino/avr/cores/arduino/wiring_pulse.c
-            ${ARDUINO_ROOT}/arduino/avr/cores/arduino/wiring_shift.c
-            ${ARDUINO_ROOT}/arduino/avr/libraries/SPI/src/SPI.cpp)
+set(src_arduino_core
+    ${ARDUINO_ROOT}/arduino/avr/cores/arduino/main.cpp
+    ${ARDUINO_ROOT}/arduino/avr/cores/arduino/CDC.cpp
+    ${ARDUINO_ROOT}/arduino/avr/cores/arduino/HardwareSerial.cpp
+    ${ARDUINO_ROOT}/arduino/avr/cores/arduino/HardwareSerial0.cpp
+    ${ARDUINO_ROOT}/arduino/avr/cores/arduino/HardwareSerial1.cpp
+    ${ARDUINO_ROOT}/arduino/avr/cores/arduino/HardwareSerial2.cpp
+    ${ARDUINO_ROOT}/arduino/avr/cores/arduino/HardwareSerial3.cpp
+    ${ARDUINO_ROOT}/arduino/avr/cores/arduino/IPAddress.cpp
+    ${ARDUINO_ROOT}/arduino/avr/cores/arduino/PluggableUSB.cpp
+    ${ARDUINO_ROOT}/arduino/avr/cores/arduino/Print.cpp
+    ${ARDUINO_ROOT}/arduino/avr/cores/arduino/Stream.cpp
+    ${ARDUINO_ROOT}/arduino/avr/cores/arduino/Tone.cpp
+    ${ARDUINO_ROOT}/arduino/avr/cores/arduino/USBCore.cpp
+    ${ARDUINO_ROOT}/arduino/avr/cores/arduino/WInterrupts.c
+    ${ARDUINO_ROOT}/arduino/avr/cores/arduino/WMath.cpp
+    ${ARDUINO_ROOT}/arduino/avr/cores/arduino/WString.cpp
+    ${ARDUINO_ROOT}/arduino/avr/cores/arduino/abi.cpp
+    ${ARDUINO_ROOT}/arduino/avr/cores/arduino/hooks.c
+    ${ARDUINO_ROOT}/arduino/avr/cores/arduino/new.cpp
+    ${ARDUINO_ROOT}/arduino/avr/cores/arduino/wiring.c
+    ${ARDUINO_ROOT}/arduino/avr/cores/arduino/wiring_pulse.S
+    ${ARDUINO_ROOT}/arduino/avr/cores/arduino/wiring_analog.c
+    ${ARDUINO_ROOT}/arduino/avr/cores/arduino/wiring_digital.c
+    ${ARDUINO_ROOT}/arduino/avr/cores/arduino/wiring_pulse.c
+    ${ARDUINO_ROOT}/arduino/avr/cores/arduino/wiring_shift.c)
 
-add_library(gamebuino
-            Gamebuino-Classic/Gamebuino.cpp
-            Gamebuino-Classic/utility/font3x5.c
-            Gamebuino-Classic/utility/settings.c
-            Gamebuino-Classic/utility/font5x7.c
-            Gamebuino-Classic/utility/font3x3.c
-            Gamebuino-Classic/utility/Buttons.cpp
-            Gamebuino-Classic/utility/Display.cpp
-            Gamebuino-Classic/utility/Sound.cpp
-            Gamebuino-Classic/utility/Battery.cpp
-            Gamebuino-Classic/utility/Backlight.cpp)
+set(src_arduino_libs
+    ${ARDUINO_ROOT}/arduino/avr/libraries/SPI/src/SPI.cpp)
 
-link_libraries(gamebuino arduino m)
+set(src_gamebuino
+    Gamebuino-Classic/Gamebuino.cpp
+    Gamebuino-Classic/utility/font3x5.c
+    Gamebuino-Classic/utility/settings.c
+    Gamebuino-Classic/utility/font5x7.c
+    Gamebuino-Classic/utility/font3x3.c
+    Gamebuino-Classic/utility/Buttons.cpp
+    Gamebuino-Classic/utility/Display.cpp
+    Gamebuino-Classic/utility/Sound.cpp
+    Gamebuino-Classic/utility/Battery.cpp
+    Gamebuino-Classic/utility/Backlight.cpp)
+
+set(GB_SRC
+    ${src_gamebuino}
+    ${src_arduino_libs}
+    ${src_arduino_core})
+
+link_libraries(m)
